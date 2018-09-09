00a557b28d918f0aa4b1cf9d155b35b8452e5908
alexisdavid <alexisduarte1512@gmail.com>
2018-09-09 15:20:57 -0500

    se crea los modelos de listas, y se empieza a trabajr la navegacion de tabs en la app

--- src/services/deseos.service.ts
+++ src/services/deseos.service.ts
@@ -1,13 +1,23 @@
 import { Injectable } from '@angular/core';
+import { Lista } from '../models';
 
 
 @Injectable()
 
 export class DeseosService{
+
+    listas: Lista[] = [];
     
     constructor(){
         console.log('deseos service inicializado');
-        
+
+
+
+        const lista1 = new Lista('recolectar piedras del infinito');
+        const lista2 = new Lista('heroes a vencer');
+
+        this.listas.push(lista1,lista2)
+        console.log('listas :',this.listas );
     }
 
 }
